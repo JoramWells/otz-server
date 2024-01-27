40ccff7b37d85a210674e78a3e0ce3e44be07062
JoramWells18 <jorammanoah1@gmail.com>
2024-01-27 21:10:09 +0300

    added nodemon.json

--- Patient/index.js
+++ Patient/index.js
@@ -9,7 +9,7 @@ const patientRoutes = require('./routes/patient.routes');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const corsOption = {
   origin: ['*'],
 };
