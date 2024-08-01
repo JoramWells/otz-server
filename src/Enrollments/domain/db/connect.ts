d845855d8af46ee3a6587d1572d0280e50b97817
JoramWells <jorammanoah1@gmail.com>
2024-08-01 07:10:36 +0300

    added a pool in connect

--- src/Enrollments/domain/db/connect.ts
+++ src/Enrollments/domain/db/connect.ts
@@ -6,20 +6,19 @@ const DB = 'otz'
 const USERNAME = 'postgres'
 const PASSWORD = 'postgres'
 
-const connect = new Sequelize(
-  DB,
-  USERNAME,
-  PASSWORD,
-  {
-    host: 'database',
-    dialect: 'postgres',
-    define: {
-      timestamps: true,
-      freezeTableName: true
-
-    }
-  }
-
-)
+const connect = new Sequelize(DB, USERNAME, PASSWORD, {
+  host: "database",
+  dialect: "postgres",
+  define: {
+    timestamps: true,
+    freezeTableName: true,
+  },
+  pool: {
+    max: 50,
+    min: 0,
+    idle: 10000,
+  },
+  // logging: false,
+});
 
 export { connect }
