e993fd43d43f83e8bd68e8d676e438b4f954f7be
JoramWells <jorammanoah1@gmail.com>
2024-09-19 10:55:24 +0300

    added google calendar

---
+++ src/Appointment/adapters/controllers/calendar.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import { google } from "googleapis";
+
+const app = express();
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URL
+);
+
+const scopes = [
+  "https://www.googleapis.com/auth/blogger",
+  "https://www.googleapis.com/auth/calendar",
+];
+
+app.get("/google", (req, res) => {
+  const url = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: scopes,
+  });
+
+  res.redirect(url)
+
+});
