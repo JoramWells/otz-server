97194033b6530e3276e7589cad89f9c66cc339d8
JoramWells <jorammanoah1@gmail.com>
2024-10-16 06:56:17 +0300

    modified notify

--- src/Notify/routes/chat/messages.routes.ts
+++ src/Notify/routes/chat/messages.routes.ts
@@ -5,6 +5,7 @@ import express from "express";
 import { MessagesRepository } from "../../adapters/repositories/chat/messageRepoitory";
 import { MessageInteractor } from "../../application/interactors/chat/messageInteractor";
 import { MessagesController } from "../../adapters/controllers/chat/messageController";
+import { createMessageValidator } from "../../adapters/validators/MessageValidator";
 
 const repository = new MessagesRepository();
 const interactor = new MessageInteractor(repository);
@@ -13,7 +14,7 @@ const controllers = new MessagesController(interactor);
 
 const router = express.Router();
 
-router.post("/add", controllers.onCreateMessage.bind(controllers));
+router.post("/add", createMessageValidator,controllers.onCreateMessage.bind(controllers));
 router.get(
   "/fetchAll",
   controllers.onGetAllMessages.bind(controllers)
